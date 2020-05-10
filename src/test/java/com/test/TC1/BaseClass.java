2526c03b49415fb7205dd2596fe1e99db1b3d8ec
atulgupta0195 <atulgupta0195@gmail.com>
2020-05-10 17:52:05 +0530

    test git file

---
+++ src/test/java/com/test/TC1/BaseClass.java
@@ -0,0 +1,36 @@
+package com.test.TC1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class BaseClass {
+	static String url = "https://chromedriver.chromium.org/capabilities";
+	WebDriver driver ;
+	
+///Tesing/DriverFile/geckodriver-v0.26.0-win64/geckodriver.exe
+	/*
+	 * public static void main(String[] args) { System.setProperty(
+	 * "webdriver.gecko.driver","F:\\WORKSPACE FOR APPUIM\\Tesing\\DriverFile\\geckodriver-v0.26.0-win64\\geckodriver.exe"
+	 * ); WebDriver driver =new FirefoxDriver();
+	 * 
+	 * //System.setProperty(
+	 * "webdriver.gecko.driver","F:\\WORKSPACE FOR APPUIM\\Tesing\\DriverFile\\chromedriver_win32\\chromedriver.exe"
+	 * );
+	 * 
+	 * //WebDriver driver = new ChromeDriver();
+	 * 
+	 * driver.get(url); }
+	 */
+	
+	@Test
+public void baseSetup() {
+	
+	System.setProperty("webdriver.gecko.driver","F:\\WORKSPACE FOR APPUIM\\Tesing\\DriverFile\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+	 driver =new FirefoxDriver();
+	 driver.get(url);
+	 driver.quit();
+}
+
+}
---
+++ src/test/java/com/test/TC1/Demo.java
@@ -0,0 +1,17 @@
+package com.test.TC1;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	/*
+	 * public static void main(String[] args) {
+	 * System.err.println("yes its working"); }
+	 */
+	@Test
+	
+	private void test() {
+		System.err.println("TESTNG .... yes its working");
+
+	}
+}
